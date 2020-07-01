16cd2ab04ad5fa3dc7c493c601f4439c7437fe27
Rodrigo Nogueira <nogueirajsx@gmail.com>
2020-07-01 16:26:17 -0300

    Action.type - Criando constantes

--- src/actions/AutenticacaoActions.js
+++ src/actions/AutenticacaoActions.js
@@ -1,71 +1,87 @@
-import firebase from 'firebase'
-import { Actions } from 'react-native-router-flux'
-import b64 from 'base-64'
+import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
+import b64 from 'base-64';
+import { 
+    MODIFICA_EMAIL,
+    MODIFICA_SENHA,
+    MODIFICA_NOME,
+    CADASTRO_USUARIO_SUCESSO,
+    CADASTRO_USUARIO_ERRO,
+    LOGIN_USUARIO_SUCESSO,
+    LOGIN_USUARIO_ERRO 
+} from './types';
 
 export const modificaEmail = (texto) => {
     return {
-        type: 'modifica_email',
+        type: MODIFICA_EMAIL,
         payload: texto
     }
 }
 
 export const modificaSenha = (texto) => {
     return {
-        type: 'modifica_senha',
+        type: MODIFICA_SENHA,
         payload: texto
     }
 }
 
 export const modificaNome = (texto) => {
     return {
-        type: 'modifica_nome',
+        type: MODIFICA_NOME,
         payload: texto
     }
 }
 
-export const cadastraUsuario = ({nome, email, senha}) => {
+export const cadastraUsuario = ({ nome, email, senha }) => {
     return dispatch => {
         firebase.auth().createUserWithEmailAndPassword(email, senha)
             .then(user => {
-                let emailB64 = b64.encode(email)
-                firebase.database().ref(`/contatos/${emailB64}` )
+                let emailB64 = b64.encode(email);
+                
+                firebase.database().ref('/contatos/'+emailB64)
                     .push({ nome })
-                    .then(value => cadastraUsuarioSucesso(dispatch))
+                    .then(value => cadastroUsuarioSucesso(dispatch))   
             })
-            .catch(erro => cadastraUsuarioErro(erro, dispatch))
+            .catch(erro => cadastroUsuarioErro(erro, dispatch));
     }
-
+    
 }
 
-const cadastraUsuarioSucesso = (dispatch) => {    
-    dispatch ({ type: 'cadastro_usuario_sucesso' })
-    Actions.boasVindas()
+
+const cadastroUsuarioSucesso = (dispatch) => {
+    dispatch ({ type: CADASTRO_USUARIO_SUCESSO });
+
+    Actions.boasVindas();
 }
 
-const cadastraUsuarioErro = (erro, dispatch) => {    
-    // console.log(erro.message)
-    dispatch ({ type: 'cadastro_usuario_erro', payload: erro.message })
+const cadastroUsuarioErro = (erro, dispatch) => {
+    dispatch ({ type: CADASTRO_USUARIO_ERRO, payload: erro.message });
 }
 
-export const autenticarUsuario = ({ email, senha }) => {    
+export const autenticarUsuario = ({ email, senha }) => {
+
     return dispatch => {
         firebase.auth().signInWithEmailAndPassword(email, senha)
             .then(value => loginUsuarioSucesso(dispatch))
-            .catch(erro => loginUsuarioErro(erro, dispatch))
+            .catch(erro => loginUsuarioErro(erro, dispatch));
     }
 }
 
 const loginUsuarioSucesso = (dispatch) => {
-    dispatch ({
-        type: 'login_usuario_sucesso'
-    })
+    dispatch (
+        {
+            type: LOGIN_USUARIO_SUCESSO
+        }
+    );
+
     Actions.principal();
 }
 
 const loginUsuarioErro = (erro, dispatch) => {
-    dispatch ({
-        type: 'login_usuario_erro',
-        payload: erro.message
-    })
+    dispatch (
+        {
+            type: LOGIN_USUARIO_ERRO,
+            payload: erro.message
+        }
+    );
 }
-
